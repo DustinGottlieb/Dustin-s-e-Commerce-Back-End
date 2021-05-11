e93ef8689dee41249a39aa1993606d1953b90ec5
dustingottlieb <dustingottlieb@gmail.com>
2021-05-10 19:18:03 -0500

    fixed tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async(req, res) => {
     // be sure to include its associated Product data
     try {
         const tagData = await Tag.findByPk(req.params.id, {
-            include: [{ model: Product, through: productTag, as: 'tagged_product' }]
+            include: [{ model: Product, through: ProductTag, as: 'tagged_product' }]
         });
 
         if (!tagData) {
@@ -29,7 +29,7 @@ router.get('/:id', async(req, res) => {
             return;
         }
 
-        res.status(200).json(productData);
+        res.status(200).json(tagData);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -68,7 +68,7 @@ router.delete('/:id', async(req, res) => {
             }
         });
 
-        if (!tagData) {
+        if (!TagData) {
             res.status(404).json({ message: 'No tag found with this id!' });
             return;
         }
